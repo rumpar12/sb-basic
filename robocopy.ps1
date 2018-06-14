451c70f62c6d4cb4ad870bad027c48e8f85f2be4
WIN-CAV2M4DMFKE\Administrator <sagar@50403038>
2018-06-13 21:58:21 -0400

    change variables

--- robocopy.ps1
+++ robocopy.ps1
@@ -1,8 +1,10 @@
 
-#robocopy e:\Test \\uswpshared01\sfacte\_keith\GBL\50.40.30.38 /e /xd Te* /copy:DAT /dcopy:T  /log+:e:\log-e.txt
-#robocopy e:\ \\uswpshared01\sfacte\_keith\GBL\50.40.30.38  /e /xd Ca* Co* cr* Dep* Ma* N* S* G* C*] /copy:DAT /dcopy:T  /log+:e:\log-e.txt
-#robocopy e:\Suggestion_cleanup \\uswpshared01\sfacte\_keith\GBL\50.40.30.38  /e /copy:DAT /dcopy:T  /log+:e:\log-e.txt
-#s.b. - created 06122018 copy everything from 54.40.30.38 D drive and copy it to network share.
+
+
+#robocopy <source>  <destination> /e /xd Te* /copy:DAT /dcopy:T  /log+:e:\log-e.txt
+#robocopy <source> <target> /e /xd Ca* Co* cr* Dep* Ma* N* S* G* C*] /copy:DAT /dcopy:T  /log+:e:\log-e.txt
+#robocopy <target> <target> /e /copy:DAT /dcopy:T  /log+:e:\log-e.txt
+#s.b. - created 06122018 copy everything from source drive to target.
 
 # Options
 # Exclude Directory recyclebin, system volumeInformation, projects
@@ -13,23 +15,21 @@
 # Retry for 100 times : /R:100 
 # append log with each iteration on source: /LOG+:robocopylog and save it on source
 #Uncomment and run below to see the log priorhand
-# robocopy D:\ \\uswpshared01\sfacte\_keith\GBL\50.40.30.38\D-Drive\   /xd "D:\`$RECYCLE.BIN\" /xd "D:\System Volume Information" /xd "D:\projects" /e /copy:DAT /dcopy:T /eta /r:100 /log+:D:/copylog.txt /L
+# robocopy <source> <target>  /xd "D:\`$RECYCLE.BIN\" /xd "D:\System Volume Information" /xd "D:\projects" /e /copy:DAT /dcopy:T /eta /r:100 /log+:D:/copylog.txt /L
 
-#For D:\ Drive
+#For system volume
 #remove system and hidden attribute Get-ItemProperty in shell to /a-:HS
-robocopy D:\ \\uswpshared01\sfacte\_keith\GBL\50.40.30.38\D-Drive\   /xd "D:\`$RECYCLE.BIN\" /xd "D:\System Volume Information" /xd "D:\projects" /e /copy:DAT /dcopy:T /a-:HS /eta /r:100 /log+:D:/copylog.txt
+robocopy <drive> <target>   /xd "D:\`$RECYCLE.BIN\" /xd "D:\System Volume Information" /xd "D:\projects" /e /copy:DAT /dcopy:T /a-:HS /eta /r:100 /log+:D:/copylog.txt
 
 #For Desktop
 
-robocopy C:\users\administrator\desktop\ \\uswpshared01\sfacte\_keith\GBL\50.40.30.38\Desktop\ /xf *.lnk /xf *.ini /xf *.website /e /copy:DAT /dcopy:T /eta /r:100 /log+:c:/users/administrator/desktop/Desktop-copylog.txt
+robocopy <source>  <target> /xf *.lnk /xf *.ini /xf *.website /e /copy:DAT /dcopy:T /eta /r:100 /log+:c:/users/administrator/desktop/Desktop-copylog.txt
 
-#Network to Network
-robocopy \\uswpshared01\sfacte\_keith\GBL\50.40.30.38\D-Drive\ \\uswpshared01\sfacte\_keith\GBL\50.40.30.38\D\ /xf *.lnk /xf *.ini /xf *.website /e /copy:DAT /dcopy:T /eta /r:100 /log+:`\\uswpshared01\sfacte\_keith\GBL\50.40.30.38\drived\
 #remove attribute system (S) and Hidden (H) so that destination does not set it to hidden and system
-robocopy Y:\D Y:\drived /e /copy:DAT /dcopy:T /a-:HS /log:c:\users\Administrator\desktop\tryd.txt /L
+robocopy <source> <target> /e /copy:DAT /dcopy:T /a-:HS /log:c:\users\Administrator\desktop\tryd.txt /L
 
-#Copy e drive to e drive
-robocopy e:\ Y:\E-Drive /e /copy:DAT /dcopy:T /a-:HS /xd "E:\`$RECYCLE.BIN\" /xd "E:\System Volume Information" /xf *.lnk /xf *.ini /log+:e:\e-backuplog.txt /L
+#Copy drive to drive
+robocopy <source> <target> /e /copy:DAT /dcopy:T /a-:HS /xd "E:\`$RECYCLE.BIN\" /xd "E:\System Volume Information" /xf *.lnk /xf *.ini /log+:e:\e-backuplog.txt /L
 
 
 ======================================END OF ROBOCOPY==============================================
